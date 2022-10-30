315c2174a37450727f9f665c5fc4d32ab6293da0
Phaedrus Raznikov <praznikov@cerego.com>
2022-10-30 13:41:36 -0400

    Close current tab when adding to hop list

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ chrome.storage.sync.get("urls", (data) => {
 // If current URL is in the list, set button text to "Remove"
 // Otherwise, set button text to "Add"
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  var currentURL = tabs[0].url;
+  const currentURL = tabs[0].url;
   if (urls.includes(currentURL)) {
     document.getElementById("add-or-remove").innerHTML = "Remove";
   } else {
@@ -21,15 +21,22 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 // Otherwise it removes the current URL from the list
 document.getElementById("add-or-remove").addEventListener("click", function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    var currentURL = tabs[0].url;
+    const currentURL = tabs[0].url;
+    let closeTab = false;
     if (urls.includes(currentURL)) {
       urls.splice(urls.indexOf(currentURL), 1);
     } else {
       urls.push(currentURL);
+      closeTab = true;
     }
 
     // Sync changes to URLs to storage
     chrome.storage.sync.set({ urls: urls.filter((url) => !!url) });
+
+    // Close tab if it was added to the list
+    if (closeTab) {
+      chrome.tabs.remove(tabs[0].id);
+    }
 
     window.close();
   });
